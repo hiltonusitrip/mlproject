cb2e0a4f632518c25ac14c1d45aee86f2a484e1a
Hilton Ma <hma68@hotmail.com>
2023-08-15 21:40:56 -0700

    change

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,6 @@ import logging
 import os
 import sys
 from datetime import datetime
-from exception import CustomException
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
@@ -18,10 +17,3 @@ logging.basicConfig(
      format = '[ %(asctime)s ] %(lineno)d - %(name)s - %(levelname)s - %(message)s'    
 )
 
-
-if __name__ == '__main__':
-    try:
-        a = 10/0
-    except Exception as e:
-        logging.error('Logging started')
-        raise CustomException(e, sys)
