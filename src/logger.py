4d4d23e0ef8eb6f06be1aab6c753e92cdd341f17
Hilton Ma <hma68@hotmail.com>
2023-08-15 18:05:48 -0700

    change

--- src/logger.py
+++ src/logger.py
@@ -6,12 +6,12 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(),'logs')
 
-os.makedirs(log_path, exist_ok=True)
+os.makedirs(logs_path, exist_ok=True)
 
-log_file_path = os.path.join(log_path, LOG_FILE)
+log_file_path = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
      filename = LOG_FILE,
      level = logging.INFO,
-     format = '[ %{asctime}s ] %{lineno}d - %{name}s - %{message}s'    
+     format = '[ %(asctime)s ] %(lineno)d - %(name)s - %(message)s'    
 )
