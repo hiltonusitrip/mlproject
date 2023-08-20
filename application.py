b6bc4bab19fcff74f5e0b1dcdae028cc806d7715
Hilton Ma <hma68@hotmail.com>
2023-08-19 22:04:50 -0700

    change

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ import pandas as pd
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 from sklearn.preprocessing import StandardScaler
 
-
 application = Flask(__name__)
 app = application
 
